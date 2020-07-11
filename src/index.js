ce451097e23cb51239d4d73676d4b4e22060a212
migueloldemburg <migueloldemburg2011@gmail.com>
2020-07-11 04:17:20 -0400

    My updates on translation on Contact Form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './i18n'; // Translation
 
 /* fonts */
 import './fonts/Galano_Grotesque.otf';
@@ -14,7 +16,11 @@ import './fonts/Galano_Grotesque_Semi_Bold.otf';
 import "../node_modules/slick-carousel/slick/slick.css"; 
 import "../node_modules/slick-carousel/slick/slick-theme.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Suspense fallback={(<div>Loading...</div>)}>
+        <App />
+    </Suspense>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/contacto/contacto.js
+++ src/pages/contacto/contacto.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import Main from './../main/main'
 import './contacto.scss'
+import { useTranslation } from 'react-i18next'
 
 function Contacto ( props ) {
+
+    const { t, i18n } = useTranslation();
+
+    function handleClick(lang){
+        i18n.changeLanguage(lang);
+    }
 
     return (
         <Main>
            <div className="container contacto">
 
-               <div className="row">
-                   <div className="col m12">
-                        <p className="contacto-title">Contacto</p>
-                   </div>
-               </div>
+                <div className="row">
+                    <div className="col m12">
+                        <p className="contacto-title">{t('Contacto')}</p>
+                    </div>
+                </div>
 
-               <div className="row">
-                   <div className="col m6 s12">
+                <div className="row">
+                    <div className="col m6 s12">
                         <div className="col m12 s6">
                             <p className="contacto-paragraph"><strong>Buenos Aires</strong></p>
                             <p className="contacto-paragraph">Sucre 632, 3er Piso</p>
@@ -33,30 +40,36 @@ function Contacto ( props ) {
                             <p className="contacto-paragraph">+54 11 4787 8700</p>
                             <br />
                         </div>
-                   </div>
-                   <div className="col m6 s12">
+                    </div>
+                    <div className="col m6 s12">
                        <form>
                            <div className="input-field form-group">
                                <input className="form-control" type="text" name="nombre" />
-                               <label>Nombre</label>
+                               <label htmlFor="nombre">{t('Nombre')}</label>
                            </div>
 
                            <div className="input-field form-group">
-                                <input className="form-control" type="email" name="mail" required="required" />
-                                <label>Mail *</label>
+                                <input className="form-control" type="email" name="email" required="required" />
+                                <label htmlFor="email">{t('Mail')} *</label>
                            </div>
 
                             <div className="input-field form-group">
-                                <textarea id="textarea1" className="materialize-textarea"></textarea>
-                                <label for="textarea1">Mensaje</label>
+                                <textarea name="mensaje" className="materialize-textarea"></textarea>
+                                <label htmlFor="mensaje">{t('Mensaje')}</label>
                             </div>
 
                             <div className="col m12 contacto-btn-submitter">
-                                <button className="btn-flat contacto-link-enviar" type="submit">ENVIAR</button>
+                                <button className="btn-flat contacto-link-enviar" type="submit">{t('ENVIAR')}</button>
                             </div>                            
                        </form>
                    </div>
-               </div>
+                </div>
+
+                <div className="row">
+                   <div className="col m12 center-align">
+                        <a className="lang-selector" onClick={()=>handleClick('en')}>EN</a> / <a className="lang-selector" onClick={()=>handleClick('es')}>ES</a>
+                   </div>
+                </div>
 
            </div>
         </Main>
